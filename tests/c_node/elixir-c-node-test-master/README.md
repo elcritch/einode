82ceed4b907330c027f3b52439d6a894be830a68
Jaremy Creechley <creechley@gmail.com>
2020-05-12 13:49:57 -0600

    reorganizing / split nim files

--- tests/c_node/elixir-c-node-test-master/README.md
+++
@@ -1,2 +0,0 @@
-# elixir-c-node-test
-Some experiments in Erlang/Elixir native interfacing with C
--- tests/c_node/elixir-c-node-test-master/include/erl_test/spawn.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#ifndef __SPAWN_H
-#define __SPAWN_H
-
-int spawn_epmd(void);
-
-#endif  // _SPAWN_H
